c0f3e46cbb910816aefe8aab4d6d3cdbe2489ee0
alip <alifrizkipambudi@gmail.com>
2017-11-16 09:36:06 +0700

    Context model & migrations done

--- migrations/20171116022128-create-context.js
+++ migrations/20171116022128-create-context.js
@@ -1,27 +1,22 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Contexts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Contexts');
-  }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Contexts', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Contexts'),
 };
--- models/context.js
+++ models/context.js
@@ -1,13 +1,18 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Context = sequelize.define('Context', {
-    name: DataTypes.STRING
+  const Context = sequelize.define('Context', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: DataTypes.STRING,
   }, {
     classMethods: {
-      associate: function(models) {
+      associate(models) {
         // associations can be defined here
-      }
-    }
+      },
+    },
   });
   return Context;
 };
