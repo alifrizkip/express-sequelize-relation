7cbbbe14187db390ebc7c5ca4b6f43772f08a06a
alip <alifrizkipambudi@gmail.com>
2017-11-21 09:07:01 +0700

    create allowFor middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,3 +1,23 @@
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 exports.checkToken = passport.authenticate('jwt', { session: false });
+
+exports.allowFor = (...allowed) => {
+  const isAllowed = role => allowed.indexOf(role) > -1;
+
+  return (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ').pop();
+    const userInfo = jwt.verify(token, 'rahasia');
+
+    if (userInfo.active === 'Y') {
+      if (isAllowed(userInfo.role)) {
+        return next();
+      }
+      return res.status(401).json('Forbidden - Not allowed');
+    }
+
+    return res.status(401).json('User suspended / locked');
+  };
+};
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,11 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/usersController');
+const { allowFor } = require('../middleware/authMiddleware');
 
-router.get('/', usersController.usersAll);
-router.get('/:id', usersController.usersDetail);
-router.post('/', usersController.usersCreate);
-router.put('/:id', usersController.usersUpdate);
-router.delete('/:id', usersController.usersDelete);
+router.get('/', allowFor('admin'), usersController.usersAll);
+router.get('/:id', allowFor('admin'), usersController.usersDetail);
+router.post('/', allowFor('admin'), usersController.usersCreate);
+router.put('/:id', allowFor('admin'), usersController.usersUpdate);
+router.delete('/:id', allowFor('admin'), usersController.usersDelete);
 
 module.exports = router;
